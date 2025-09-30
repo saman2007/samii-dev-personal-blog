32359eb9f737845e60d37f95a3932cd3eeb0a655
sami_dev <samanahrari@yahoo.com>
2025-09-30 11:12:47 +0330

    FEATURE: next font and modified some css vars

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,20 @@
+import { Inter, Vazirmatn } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  weight: ["400", "500", "700", "900"],
+  fallback: ["Roboto"],
+});
+
+const vazir = Vazirmatn({
+  subsets: ["arabic"],
+  variable: "--font-vazir",
+  weight: ["400", "500", "700", "900"],
+  fallback: ["Roboto"],
+});
 
 export default function RootLayout({
   children,
@@ -6,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn(inter.variable, vazir.variable)}>
       <body>{children}</body>
     </html>
   );
