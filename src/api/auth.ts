6fd5180cf5c1f8714829dd6eb7ebc1f9802054bf
sami_dev <samanahrari@yahoo.com>
2025-10-31 23:14:04 +0330

    FEATURE: implemented sign out api and used it to sign out user in client side

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -5,6 +5,7 @@ import { APIResponse, UserPrivateInfo } from "@/types/types";
 const SIGN_IN_PATH: string = "/api/sign-in";
 const SIGN_UP_PATH: string = "/api/sign-up";
 const USER_PRIVATE_INFO_PATH: string = "/api/user-private-info";
+const SIGN_OUT_PATH: string = "/api/sign-out";
 
 export const signInAPI = async (data: SignInAPIBody) => {
   return await axiosInstance.post<APIResponse<UserPrivateInfo>>(
@@ -24,4 +25,8 @@ export const userPrivateInfoAPI = async () => {
   return await axiosInstance.get<APIResponse<UserPrivateInfo>>(
     USER_PRIVATE_INFO_PATH
   );
+};
+
+export const signOutAPI = async () => {
+  return await axiosInstance.get<APIResponse<null>>(SIGN_OUT_PATH);
 };
--- src/components/NavigationBar/AvatarItem/AvatarItem.tsx
+++ src/components/NavigationBar/AvatarItem/AvatarItem.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { signOutAPI } from "@/api/auth";
 import Link from "@/components/Link/Link";
 import { Avatar, AvatarImage } from "@/components/UI/Avatar/Avatar";
 import {
@@ -10,7 +11,12 @@ import {
   DropdownMenuSeparator,
 } from "@/components/UI/DropDownMenu/DropDownMenu";
 import { Skeleton } from "@/components/UI/Skeleton/Skeleton";
+import { Spinner } from "@/components/UI/Spinner/Spinner";
+import { useSetStore } from "@/contexts/storeContext";
+import { useRequest } from "@/hooks/useRequest";
 import { ChevronDown, LogOut, Settings, User } from "lucide-react";
+import { MouseEventHandler } from "react";
+import { toast } from "sonner";
 
 export interface AvatarItemProps {
   isLoading?: boolean;
@@ -18,10 +24,24 @@ export interface AvatarItemProps {
 }
 
 const AvatarItem = ({ isLoading = false, avatarImg }: AvatarItemProps) => {
+  const setStore = useSetStore();
+  const { execute: signOut, isLoading: isSigningOut } = useRequest(signOutAPI);
+
   if (isLoading)
     return (
       <Skeleton className="cursor-pointer h-10 rounded-full bg-block flex group w-20 justify-between items-center" />
     );
+
+  const onSignOutHandler: MouseEventHandler<HTMLDivElement> = async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    await signOut();
+
+    setStore({ auth: { isLoading: false, user: null, isLoggedIn: false } });
+
+    toast.success("You signed out successfully.");
+  };
 
   return (
     <DropdownMenu>
@@ -34,22 +54,31 @@ const AvatarItem = ({ isLoading = false, avatarImg }: AvatarItemProps) => {
         </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-56">
-        <DropdownMenuItem asChild className="cursor-pointer">
+        <DropdownMenuItem asChild className="cursor-pointer transition-colors">
           <Link href="/profile" className="flex items-center group">
-            <User className="mr-2 h-4 w-4 text-text-secondary group-hover:text-semi-white" />
+            <User className="mr-2 h-4 w-4 text-text-secondary group-hover:text-semi-white transition-colors" />
             <span>Profile</span>
           </Link>
         </DropdownMenuItem>
-        <DropdownMenuItem asChild className="cursor-pointer">
+        <DropdownMenuItem asChild className="cursor-pointer transition-colors">
           <Link href="/settings" className="flex items-center group">
-            <Settings className="mr-2 h-4 w-4 text-text-secondary group-hover:text-semi-white" />
+            <Settings className="mr-2 h-4 w-4 text-text-secondary group-hover:text-semi-white transition-colors" />
             <span>Settings</span>
           </Link>
         </DropdownMenuItem>
         <DropdownMenuSeparator />
-        <DropdownMenuItem className="cursor-pointer text-red focus:text-red group">
-          <LogOut className="mr-2 h-4 w-4 text-text-secondary group-hover:text-semi-white" />
-          <span>Sign out</span>
+        <DropdownMenuItem
+          className="cursor-pointer text-red focus:text-red group transition-colors"
+          onClick={onSignOutHandler}
+        >
+          {isSigningOut ? (
+            <Spinner className="size-5 m-auto text-text-secondary group-hover:text-semi-white transition-colors" />
+          ) : (
+            <>
+              <LogOut className="mr-2 h-4 w-4 text-text-secondary group-hover:text-semi-white transition-colors" />
+              <span>Sign out</span>
+            </>
+          )}
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
